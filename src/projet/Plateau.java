335f056117201bf706bddd4b3e128b20bb7e6bd1
Arnaud Leveau <arnaud@MacBook-Pro-de-Arnaud.local>
2019-11-11 21:00:39 +0100

    methode ajout bateau verti et hori

--- src/projet/Plateau.java
+++ src/projet/Plateau.java
@@ -16,9 +16,9 @@ public class Plateau {
 		nbCol = c;
 		plateau = new char[nbLig][nbCol];
 		
-		for(int i=0; i<nbLig; i++) {
-			for(int j=0; j<nbCol; j++) {
-				plateau[i][j] = 'x';
+		for(int x=0; x<nbLig; x++) {
+			for(int y=0; y<nbCol; y++) {
+				plateau[x][y] = 'x';
 			}
 		}
 	}
@@ -36,13 +36,59 @@ public class Plateau {
 		System.out.println("Touché coulé explose");
 		System.out.println();
 		afficheSeparateur(nbCol);
-		for(int i=0; i<nbLig; i++) {
-			for(int j=0; j<nbCol; j++) {
-				System.out.print(" | " + plateau[i][j]);
+		for(int x=0; x<nbLig; x++) {
+			for(int y=0; y<nbCol; y++) {
+				System.out.print(" | " + plateau[x][y]);
 			
 			}
 			System.out.println(" | ");
 			afficheSeparateur(nbCol);
+		}
+	}
+	
+	public void placerHori(int x, int y, int t) {
+		x = x-1;
+		y = y-1;
+		
+		
+		// si le bateau se trouve sur le grille
+		if (x<0 || y<0 || x>nbCol || y>nbLig) {
+			System.out.print("Erreur le plateau est hors zone de jeu !");
+			return;
+		}
+		// teste si cellule est vide
+		for(int i=0; i<t; i++ ) {
+			if(plateau[x][y+i]!='x') {
+				System.out.print("Erreur un bateau se trouve déja à cet endroit !");
+				return;
+			}
+		}
+		for(int i=0; i<t; i++ ) {
+			plateau[x][y+i] = 'B';
+			afficherConsole();
+		}
+	}
+	
+	public void placerVerti(int x, int y, int t) {
+		x = x-1;
+		y = y-1;
+		
+		
+		// si le bateau se trouve sur le grille
+		if (x<0 || y<0 || x>nbCol || y>nbLig) {
+			System.out.print("Erreur le plateau est hors zone de jeu !");
+			return;
+		}
+		// teste si cellule est vide
+		for(int i=0; i<t; i++ ) {
+			if(plateau[x+i][y]!='x') {
+				System.out.print("Erreur un bateau se trouve déja à cet endroit !");
+				return;
+			}
+		}
+		for(int i=0; i<t; i++ ) {
+			plateau[x+i][y] = 'B';
+			afficherConsole();
 		}
 	}
 
--- src/projet/main.java
+++ src/projet/main.java
@@ -1,9 +1,15 @@
 package projet;
 
+import java.util.*;
+
 public class main {
 
 		public static void main(String[] args) {
 			Plateau p1 = new Plateau(10, 10);
 			p1.afficherConsole();
+			// ajoute un bateau hori
+			p1.placerHori(1,3,4);
+			// ajoute un bateau verti
+			p1.placerVerti(1,2,3);
 		}
 }
