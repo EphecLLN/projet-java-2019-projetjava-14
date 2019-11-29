36a07a403eb59f84f5f9cd74092463be99aa5263
Noscuro52 <55975884+Noscuro52@users.noreply.github.com>
2019-11-29 15:25:14 +0100

    Add files via upload

---
+++ projet-java-2019-projetjava-14.git/src/projet/Shoots.java
@@ -0,0 +1,27 @@
+package projet;
+
+public class Shoots {
+	
+public static void feu(int x, int y, Plateau joueur) {
+	System.out.println("test");
+		
+		x = x-1;
+		y = y-1;
+		char coord = joueur.getPlateau()[y][x];
+				
+			if( coord == 'B') {
+			
+				coord = 'T';
+				System.out.print("Touché en "+ x +" : " + y);
+				return;
+			}
+			else {
+				coord = 'O' ;
+				System.out.print("Il n'y a pas de bateau à cette position");
+			return;
+			
+			}
+}
+
+}
+
